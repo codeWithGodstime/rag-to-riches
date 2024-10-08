4888b4989b388d2a4e617ac37251fadb079333f3
codeWithGodstime <specialgodstym@gmail.com>
2024-10-08 17:00:48 +0100

    modification

--- expense/urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from .views import ExpenseViewset, GoalViewset
-
-router = SimpleRouter()
-router.register("expenses", ExpenseViewset, "expenses")
-router.register("goals", GoalViewset, "goals")
-
-urlpatterns = router.urls
